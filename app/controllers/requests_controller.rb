73fabcee17243d9715ebc830450e2e9b1ff56145
Eric Richardson <eric@ericonrails.com>
2016-06-13 09:39:26 -0700

    Display requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,5 @@
 class RequestsController < ApplicationController
-
+  before_action :find_request, only: [:show]
   def new
     @request = Request.new
   end
@@ -7,15 +7,26 @@ class RequestsController < ApplicationController
   def create
     @request = Request.new request_params
     if @request.save
-      render text: "Made it"
+      redirect_to request_path @request
     else
-      render text: "FAIL"
+      render :new
     end
+  end
+
+  def show
+  end
+
+  def index
+    @requests = Request.order(created_at: :desc)
   end
 
   private
 
     def request_params
       params.require(:request).permit(:name, :email, :department, :message )
+    end
+
+    def find_request
+      @request = Request.find params[:id]
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   get '/requests/new' => 'requests#new', as: :new_request
   post '/request' => 'requests#create', as: :requests
+  get '/requests/:id' => 'requests#show', as: :request
+  get '/requests' => 'requests#index'
 
 end
