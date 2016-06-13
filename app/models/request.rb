d021b935079f8346ecb245444d1577f655cd9820
Eric Richardson <eric@ericonrails.com>
2016-06-13 12:41:15 -0700

    Validations

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,7 @@
 class Request < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :message, presence: true
 
   scope :search, -> (term) { where("name LIKE :term OR email LIKE :term OR department LIKE :term OR message LIKE :term", {term: "%#{term}%"})}
 end
